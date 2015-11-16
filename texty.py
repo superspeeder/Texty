0976934341decd6813002fa191b85b827204a979
Andy Newton <andy@delusion.org>
2015-11-15 18:10:06 -0800

    fix spelling errors

--- texty.py
+++ texty.py
@@ -39,14 +39,14 @@ def before_request():
 
 @app.teardown_request
 def teardown_request(exception):
-    b = getattr(g,'db',None)
+    db = getattr(g,'db',None)
     if db is not None:
         db.close()
 
 @app.route('/')
 def show_entries():
     cur = g.db.execute('select title, text from entries order by id desc')
-    entries = [dict(title=row[0], text = row[1]) for row in cr.fetchall()]
+    entries = [dict(title=row[0], text = row[1]) for row in cur.fetchall()]
     return render_template('show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
